16fc01a4d9c04d7f5935877acb17dbada1d9d53d
Amaan Ali <amaanali2002@hotmail.com>
2024-04-16 19:27:56 -0400

    a6

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// const express = require("express");
+import "dotenv/config";
 import express from "express";
 import Hello from "./Hello.js";
 import Courses from "./Courses/routes.js";
@@ -10,23 +10,34 @@ import Users from "./Users/routes.js";
 import Lab5 from "./Lab5.js";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING =
+    process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
     cors({
-        origin: "https://a5--splendorous-selkie-55b336.netlify.app",
+        origin: process.env.FRONTEND_URL,
+        // origin: "https://a5--splendorous-selkie-55b336.netlify.app",
         credentials: true,
     })
 );
 app.use(express.json());
-app.use(
-    session({
-        secret: "secret",
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: false },
-    })
-);
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
 
 Hello(app);
 Courses(app);
